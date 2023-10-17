5e6cedb65530454c7866bb09cff5cdbc0a48d8b5
andmansim <91722324+andmansim@users.noreply.github.com>
2023-10-17 13:32:22 +0200

    cosa

--- cosa.py
+++ cosa.py
@@ -252,3 +252,44 @@ print(f"Texto generado: {sequence}")
 beam_search(input_ids, 0, bar, length, beams, 'top_k', 1)
 sequence, max_score = get_best_sequence(graph)
 print(f"Texto generado: {sequence}")
+
+#nucleus sampling
+def nucleus_sampling(logits, temperature, p, beams, plot=True):
+    assert p > 0
+    assert p <= 1
+
+    # Ordenar las probabilidades en orden descendente y calcular las probabilidades acumulativas
+    sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+    probabilities = torch.nn.functional.softmax(sorted_logits / temperature, dim=-1)
+    cumulative_probabilities = torch.cumsum(probabilities, dim=-1)
+
+    # Crear una máscara para las probabilidades que se encuentran en el top-p
+    mask = cumulative_probabilities < p
+
+    # Si no hay un índice donde cumulative_probabilities < p, utilizamos en su lugar los mejores n tokens
+    if mask.sum() > beams:
+        top_p_index_to_keep = torch.where(mask)[0][-1].detach().cpu().tolist()
+    else:
+        top_p_index_to_keep = beams
+
+    # Mantener solo los índices top-p
+    indices_to_remove = sorted_indices[top_p_index_to_keep:]
+    sorted_logits[indices_to_remove] = float('-inf')
+
+    # Muestrear n tokens de la distribución resultante
+    probabilities = torch.nn.functional.softmax(sorted_logits / temperature, dim=-1)
+    next_tokens = torch.multinomial(probabilities, beams)
+
+    # Graficar la distribución
+    if plot:
+        total_prob = torch.nn.functional.softmax(logits / temperature, dim=-1)
+        plot_prob_distribution(total_prob, next_tokens, 'núcleo', top_p_index_to_keep)
+
+    return next_tokens
+
+# Comenzar a generar texto con búsqueda 'núcleo'
+beam_search(input_ids, 0, bar, length, beams, 'núcleo', 1)
+sequence, max_score = get_best_sequence(graph)
+print(f"Texto generado: {sequence}")
+# Graficar el grafo
+plot_graph(graph, length, beams, 'secuencia')
