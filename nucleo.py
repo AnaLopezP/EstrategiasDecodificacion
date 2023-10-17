0853a4f3fa474f3de2605fd98fb99b56ceb28fc7
andmansim <91722324+andmansim@users.noreply.github.com>
2023-10-17 13:00:09 +0200

    cosa

--- nucleo.py
+++ nucleo.py
@@ -1,4 +1,35 @@
 
+# Make sure we're using UTF-8 as encoding
+import locale
+locale.getpreferredencoding = lambda: "UTF-8"
+
+# Set seed
+import torch
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+torch.cuda.manual_seed_all(42)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+#background
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import torch
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+model = GPT2LMHeadModel.from_pretrained('gpt2').to(device)
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model.eval()
+
+text = "I have a dream"
+input_ids = tokenizer.encode(text, return_tensors='pt').to(device)
+
+outputs = model.generate(input_ids, max_length=len(input_ids.squeeze())+5)
+generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+print(f"Generated text: {generated_text}")
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+import time
 
 #nucleus sampling
 def nucleus_sampling(logits, temperature, p, beams, plot=True):
